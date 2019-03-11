785d8b09d72883d3d00210e4f16ac1edb481b995
Mike Enochs <msenochs@umich.edu>
2019-03-11 12:35:51 -0400

    Added Error Handling for PO# User Input
    Added a function - validate_po() - that takes the PO# entered by the user and validates that it is a 10 digit string.  If it is not, it prints a message indicating that the PO is incorrect and prompts the user to re-enter the PO#.  #2

--- hp_order_status.py
+++ hp_order_status.py
@@ -38,15 +38,15 @@ driver = webdriver.Chrome(chrome_path)
 driver.get("https://wolverineaccess.umich.edu/marketsite_alert.html")
 driver.find_element_by_xpath("""//*[@id="content_static"]/p[2]/a/img""").click()
 
-#username = driver.find_element_by_id("login")
-#password = driver.find_element_by_id("password")
 
+# Clears out the uniqname and password fields if login attempt unsuccessful
 def clear_uniqname():
     blank_pass = driver.find_element_by_id("password")
     blank_pass.clear()
     blank_uniqname = driver.find_element_by_id("login")
     blank_uniqname.clear()
 
+# Allows user to login when prompted to access order info
 def user_login():
     user_name_input = input("Enter uniqname: ")
     user_password_input = getpass.getpass("Password: ")
@@ -84,8 +84,6 @@ driver.find_element_by_id("sticker-1459161").click()
 WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"""/html/frameset/frame[2]""")))
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, """/html/body/table[5]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr[5]/td[2]/h3/a""")))
 
-#driver.implicitly_wait(10)
-
 driver.find_element_by_link_text("Order status").click()
 
 # Clicking on order status link generates a new window so we must switch to it
@@ -102,17 +100,22 @@ def clear_search():
     blank = driver.find_element_by_id("compSearchId")
     blank.clear()
     
-user_po_entry = False
-def validate_po():
-    if len(user_po_entry) == 10:
-        return user_po_entry
+# Validates the PO entered is a 10 digit string
+user_po_entry = ''
+def validate_po(user_po_entry):
+    import re
+    if not re.match("^[0-9]*$", user_po_entry):
+        print("The PO# must contain only digits.")
+        po_search(user_po_entry)
+    elif len(user_po_entry) != 10:
+        print("The PO# must be 10 digits long.")
+        po_search(user_po_entry)
     else:
-        print("This does not appear to be a valid HP PO#.  Please try again.")
+        return user_po_entry
 
 def po_search(user_po_entry):
     user_po_entry = input("Enter HP PO#: ")
-    print(type(user_po_entry))
-    validate_po()
+    validate_po(user_po_entry)
     poNum = driver.find_element_by_id("compSearchId")
     
     poNum.send_keys(user_po_entry)
@@ -170,12 +173,6 @@ def po_search(user_po_entry):
         print("Item Description: " + item_description1)
         order_quantity1 = item_descriptions[item][28]
         print("Order Quantity: " + order_quantity1)
-#        if type(item_descriptions[item][28]) == int:
-#            order_quantity1 = item_descriptions[item][28]
-#            print("Order Quantity: " + order_quantity1)
-#        elif type(item_descriptions[item][28]) == str:
-#            cancelled_item = str(item_descriptions[item][28])
-#            print("Order Quantity: " + cancelled_item)
         if item_status1 == 'Production':
             open_items1 = item_descriptions[item][32]
             print("Open Quantity: " + open_items1)
